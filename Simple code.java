e7b42b17b789017dd98f08c84d3bd3cf3969cb34
Kaviya R <kaviyaraja00@gmail.com>
2026-04-06 07:07:03 +0530

    Create Simple code.java

---
+++ Simple code.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, I'm Practicing Java");
+		
+		String name = "Kaviya";
+		int age = 20;
+		System.out.println("My  name is "+ name);
+		System.out.println("My age is "+ age);
+
+	}
+
+}
